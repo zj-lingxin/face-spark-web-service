fdc9ce4677233c430f058c0f5676f34be2a1878e
zj-lingxin <zj_lingxin@163.com>
2016-06-20 10:49:29 +0800

    update

--- src/main/scala/com/zjlp/face/spark/service/impl/BusinessCircleImpl.scala
+++ src/main/scala/com/zjlp/face/spark/service/impl/BusinessCircleImpl.scala
@@ -84,32 +84,6 @@ class BusinessCircleImpl extends IBusinessCircle with Logging {
     resultList
   }
 
-  def searchCommonFriendNum2(userNames: util.List[String], loginAccount: String): util.List[CommonFriendNum] = {
-
-    val beginTime = System.currentTimeMillis()
-    val sqlContext: SQLContext = sparkBaseFactory.getSQLContext
-
-    //得到userNames的朋友 (朋友，username)
-    val othersFriends: RDD[(String, String)] = sqlContext.sql(s"select username,loginAccount from ofRoster where username in ('${userNames.mkString("','")}') and loginAccount != '$loginAccount' and username != loginAccount")
-      .map(a => (a(0).toString, a(1).toString))
-
-    //得到loginAccount的朋友
-    val myFriends = sqlContext.sql(s"select loginAccount from ofRoster where username = '$loginAccount'").map(_ (0).toString).collect()
-
-    //该步骤会计算出共同好友的人数，但是如果共同好友人数为0，则username会被过滤掉
-    val comFriendsMap = othersFriends.aggregateByKey(new ArrayBuffer[String](), Props.get("spark.default.parallelism").toString.toInt)((acc: ArrayBuffer[String], value: String) => acc += value, (acc1, acc2) => acc1 ++ acc2)
-      .mapValues(a => a.distinct.intersect(myFriends).size).collectAsMap()
-
-    val resultList = new util.ArrayList[CommonFriendNum]()
-    userNames.foreach { username =>
-      val num = if (comFriendsMap.contains(username)) comFriendsMap(username) else 0
-      resultList.add(new CommonFriendNum(username, num))
-    }
-
-    logInfo(s"searchCommonFriendNum耗時:${(System.currentTimeMillis() - beginTime) / 1000D} s")
-
-    resultList
-  }
 
   /**
    * 根据当前登录用户id和用户id列表返回人脉关系类型列表
@@ -118,7 +92,6 @@ class BusinessCircleImpl extends IBusinessCircle with Logging {
    * @return
    */
   override def searchPersonRelation(userIds: util.List[String], loginAccount: String): util.List[PersonRelation] = {
-
     val beginTime = System.currentTimeMillis()
     val sqlContext: SQLContext = sparkBaseFactory.getSQLContext
 
@@ -143,11 +116,9 @@ class BusinessCircleImpl extends IBusinessCircle with Logging {
     sqlContext.dropTempTable(tableNameA)
 
     val list = new util.ArrayList[PersonRelation]()
-    val stranger = userIds.filter(a => !(oneLevelFriendsArray.contains(a) || twoLevelFriendsArray.contains(a)))
 
     oneLevelFriendsArray.foreach(a => list.add(new PersonRelation(a, 1)))
     twoLevelFriendsArray.foreach(a => list.add(new PersonRelation(a, 2)))
-    stranger.foreach(a => list.add(new PersonRelation(a, 3)))
 
     logInfo(s"searchPersonRelation耗時:${(System.currentTimeMillis() - beginTime) / 1000D} s")
     list
